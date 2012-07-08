5a2ac9080e0b9d1676f49ab60135c6055a5f3789
opkorz <rodxanderbondoc@gmail.com>
2012-07-08 19:45:34 +0800

    deleted env

--- env/lib/python2.7/site-packages/endless_pagination/exceptions.py
+++
@@ -1,1 +0,0 @@
-class PaginationError(Exception): pass
--- env/www/studentplanner/studentplanner/core/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse, HttpResponseRedirect
-
-def start_up(request):
-	response = '<html><title>Student Planner</title> <center><strong><h1>SITE UNDER CONSTRUCTION!</h1><br><br></strong></center><body>This site will be constructed as a completion to the CS172 class of UP Diliman in cooperation with Google and AITI-MIT. The project is called S-Plan, short for student planner, which is an application that will help students organize their priorities for the whole term. The said app can also suggest activities for the student during his/her free time.</body></html>'
-	return HttpResponse(response)
