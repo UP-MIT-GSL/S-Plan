f367ed80a350f19532f5dad4aab2d74462ee0608
opkorz <rodxanderbondoc@gmail.com>
2012-07-09 22:08:24 +0800

    minor change

--- env/www/studentplanner/studentplanner/settings.py
+++ env/www/studentplanner/studentplanner/settings.py
@@ -14,8 +14,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'dev',                      # Or path to database file if using sqlite3.
-        'USER': 'Xander',                      # Not used with sqlite3.
-        'PASSWORD': 'kroz09',                  # Not used with sqlite3.
+        'USER': 'admin',                      # Not used with sqlite3.
+        'PASSWORD': 'admin',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
