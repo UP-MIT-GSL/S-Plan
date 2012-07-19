41fdfa5f5e87b38d6ec70cbf4f8cfcbc5d56eda6
opkorz <rodxanderbondoc@gmail.com>
2012-07-19 16:46:32 +0800

    Edited models

--- env/www/studentplanner/studentplanner/core/models.py
+++ env/www/studentplanner/studentplanner/core/models.py
@@ -15,27 +15,6 @@ class UserAccount(models.Model):
     webmail = models.CharField(max_length=100)
     def __unicode__(self):
         return "%s"%username
-class Tasks(models.Model):
-    task = models.CharField(max_length=50)
-    start= models.DateField(auto_now_add = True)
-    end= models.DateField()
-    subject = models.CharField(max_length=50)
-    def __unicode__(self):
-        return "%s"%task
-class Event(models.Model):
-    event = models.CharField(max_length=50)
-    start= models.DateField(auto_now_add = True)
-    end= models.DateField()
-    time = models.TimeField()
-    venue = models.CharField(max_length=150)
-    def __unicode__(self):
-        return "%s"%event
-class Reminder(models.Model):
-    reminder = models.CharField(max_length=50)
-    start= models.DateField(auto_now_add = True)
-    end= models.DateField()
-    def __unicode__(self):
-        return "%s"%reminder
 class Notes(models.Model):
     note = models.CharField(max_length=50)
     body = models.CharField(max_length=5000)
@@ -43,3 +22,19 @@ class Notes(models.Model):
     end= models.DateField()
     def __unicode__(self):
         return "%s"%note
+class Tasks(models.Model):
+    event= models.ForeignKey(Notes, related_name="Reminder")
+    subject = models.CharField(max_length=50)
+    def __unicode__(self):
+        return "%s"%task
+class Event(models.Model):
+    event= models.ForeignKey(Notes, related_name="Reminder")
+    time = models.TimeField()
+    venue = models.CharField(max_length=150)
+    def __unicode__(self):
+        return "%s"%event
+class Reminder(models.Model):
+    reminder= models.ForeignKey(Notes, related_name="Reminder")
+    def __unicode__(self):
+        return "%s"%reminder
+
