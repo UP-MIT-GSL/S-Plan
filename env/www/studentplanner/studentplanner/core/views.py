c1035cf5c395ee82c2c2253dd54979b82ab117c2
opkorz <rodxanderbondoc@gmail.com>
2012-07-22 23:29:06 +0800

    Minor changes

--- env/www/studentplanner/studentplanner/core/views.py
+++ env/www/studentplanner/studentplanner/core/views.py
@@ -5,11 +5,20 @@ def start_up(request):
     html='<html><h1><strong><center>SITE UNDER CONSTRUCTION</center></strong></h1><br><p></p></html>'
     return HttpResponse(html)
 def login(request):
+    return None
 def profile(request, username):
+    return None
 def show_task(request):
+    return None
 def show_events(request):
+    return None
 def show_notes(request):
+    return None
 def show_reminders(request):
+    return None
 def clear_done_tasks(request):
+    return None
 def logout(request):
+    return None
 def show_calendar(request):
+    return None
--- env/www/studentplanner/studentplanner/urls.py
+++ env/www/studentplanner/studentplanner/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from studentplanner.core import views
+from django.views.generic import TemplateView
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -14,5 +15,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.start_up, name="start_up"),
+    url(r'^$', TemplateView.as_view(template_name="base.html"), name="base"),
 )
