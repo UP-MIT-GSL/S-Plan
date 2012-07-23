de2292b209be613914c2fe030709c0f2bb62cd10
opkorz <rodxanderbondoc@gmail.com>
2012-07-23 22:43:24 +0800

    Fixed base template, ready for controller modifications

--- env/www/studentplanner/studentplanner/settings.py
+++ env/www/studentplanner/studentplanner/settings.py
@@ -63,12 +63,12 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static-raw/'
+STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
--- env/www/studentplanner/studentplanner/urls.py
+++ env/www/studentplanner/studentplanner/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from studentplanner.core import views
 from django.views.generic import TemplateView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+import settings
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -17,3 +19,11 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', TemplateView.as_view(template_name="base.html"), name="base"),
 )
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    )
