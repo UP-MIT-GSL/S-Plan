dc2f7ed57d6009a0a375854fc5465a10bbb6c1a5
opkorz <rodxanderbondoc@gmail.com>
2012-07-28 13:31:58 +0800

    Fixed views, ready for testing

--- env/www/studentplanner/studentplanner/core/views.py
+++ env/www/studentplanner/studentplanner/core/views.py
@@ -30,7 +30,7 @@ def login1(request):
         else:
             return render_to_response('login.html',{'username':username},context_instance=RequestContext(request))
     else:
-        return HttpResponseRedirect('')
+        return HttpResponseRedirect(reverse('home'))
 def logout1(request):
     if request.user.is_authenticated()==True:
         try:
@@ -40,36 +40,56 @@ def logout1(request):
             pass
         return HttpResponseRedirect(reverse('login'))
     else:
-        return HttpResponseRedirect()
+        return HttpResponseRedirect(reverse('login'))
 def profile(request):
-    tempUser = User.objects.get(username__exact = request.session['username'])
-    return None
+    if request.user.is_authenticated() == True:
+        tempUser = User.objects.get(username__exact = request.session['username'])
+        useraccount = OrgAccount.objects.get(User = tempUser)
+        if useraccount == None:
+            useraccount = UserAccount.objects.get(User = tempUser) 
+        return render_to_response('profile.html',{'tempUser':tempUser, 'useraccount':useraccount},context_instance=RequestContext(request))
+    else:
+        return HttpResponseRedirect(reverse('login'))    
 def show_task(request):
     if request.user.is_authenticated() == True:
         tempUser = User.objects.get(username__exact = request.session['username'])
         task_list = Task.objects.filter(owner = tempUser)
+        clear_done_tasks(request)
+        return render_to_response('tasks.html',{'task_list':task_list},context_instance=RequestContext(request))
     else:
-        return HttpResponseRedirect()
+        return HttpResponseRedirect(reverse('login'))
 def show_events(request):
     if request.user.is_authenticated() == True:
         tempUser = User.objects.get(username__exact = request.session['username'])
         event_list = Event.objects.filter(owner = tempUser)
+        return render_to_response('events.html',{'event_list':event_list},context_instance=RequestContext(request))
     else:
-        return HttpResponseRedirect()
+        return HttpResponseRedirect(reverse('login'))
 def show_notes(request):
     if request.user.is_authenticated() == True:
         tempUser = User.objects.get(username__exact = request.session['username'])
         note_list = Note.objects.filter(owner = tempUser)
+        return render_to_response('tasks.html',{'task_list':task_list},context_instance=RequestContext(request))
     else:
-        return HttpResponseRedirect()
+        return HttpResponseRedirect(reverse('login'))
 def show_reminders(request):
     if request.user.is_authenticated() == True:
         tempUser = User.objects.get(username__exact = request.session['username'])
         reminder_list = Reminder.objects.filter(owner = tempUser)
+        return render_to_response('reminders.html',{'reminder_list':reminder_list},context_instance=RequestContext(request))
     else:
-        return HttpResponseRedirect()
+        return HttpResponseRedirect(reverse('login'))
 def clear_done_tasks(request):
-    return None
+    if request.user.is_authenticated() == True:
+        tempUser = User.objects.get(username__exact = request.session['username'])
+        task_list1= Task.ojbects.filter(owner = tempUser)
+        task_list2= []
+        for a in task_list:
+            if a.isDone == False:
+                task_list2.append(a)
+        return render_to_response('tasks.html',{'task_list2':task_list2},context_instance=RequestContext(request))
+    else
+        return HttpResponseRedirect(reverse('login'))    
 def show_calendar(request):
 	#if request.user.is_authenticated() = True:
 		#tempUser = User.objects.get(username__exact = request.session['username'])
