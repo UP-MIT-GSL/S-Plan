3bfe744d819ad7c63b14484098b3cd0086d5bebf
opkorz <rodxanderbondoc@gmail.com>
2012-07-29 09:00:57 +0800

    Minor changes

--- env/www/studentplanner/studentplanner/core/views.py
+++ env/www/studentplanner/studentplanner/core/views.py
@@ -21,7 +21,7 @@ def login1(request):
                 if user.is_active:
                     login(request,user)
                     request.session['username']=username            
-                    return HttpResponseRedirect('/home')
+                    return HttpResponseRedirect(reverse('home'))
             else:
                 return HttpResponseRedirect(reverse('login'))
         else:
@@ -40,7 +40,7 @@ def logout1(request):
         return HttpResponseRedirect(reverse('login'))
 def profile(request):
     if request.user.is_authenticated() == True:
-        tempUser = User.objects.get(username__exact = request.session['username'])
+        tempUser = User.objects.get(username= request.session['username'])
         useraccount = OrgAccount.objects.get(User = tempUser)
         if useraccount == None:
             useraccount = UserAccount.objects.get(User = tempUser) 
@@ -49,7 +49,7 @@ def profile(request):
         return HttpResponseRedirect(reverse('login'))    
 def show_tasks(request):
     if request.user.is_authenticated() == True:
-        tempUser = User.objects.get(username__exact = request.session['username'])
+        tempUser = User.objects.get(username = request.session['username'])
         task_list = Task.objects.filter(owner = tempUser)
         clear_done_tasks(request)
         return render_to_response('tasks.html',{'task_list':task_list},context_instance=RequestContext(request))
@@ -57,14 +57,14 @@ def show_tasks(request):
         return HttpResponseRedirect(reverse('login'))
 def show_events(request):
     if request.user.is_authenticated() == True:
-        tempUser = User.objects.get(username__exact = request.session['username'])
+        tempUser = User.objects.get(username = request.session['username'])
         event_list = Event.objects.filter(owner = tempUser)
         return render_to_response('events.html',{'event_list':event_list},context_instance=RequestContext(request))
     else:
         return HttpResponseRedirect(reverse('login'))
 def show_notes(request):
     if request.user.is_authenticated() == True:
-        tempUser = User.objects.get(username__exact = request.session['username'])
+        tempUser = User.objects.get(username = request.session['username'])
         task_list = Task.objects.filter(owner = tempUser)
         event_list = Event.objects.filter(owner = tempUser)
         reminder_list = Reminder.objects.filter(owner = tempUser)
@@ -73,14 +73,14 @@ def show_notes(request):
         return HttpResponseRedirect(reverse('login'))
 def show_reminders(request):
     if request.user.is_authenticated() == True:
-        tempUser = User.objects.get(username__exact = request.session['username'])
+        tempUser = User.objects.get(username = request.session['username'])
         reminder_list = Reminder.objects.filter(owner = tempUser)
         return render_to_response('reminders.html',{'reminder_list':reminder_list},context_instance=RequestContext(request))
     else:
         return HttpResponseRedirect(reverse('login'))
 def clear_done_tasks(request):
     if request.user.is_authenticated() == True:
-        tempUser = User.objects.get(username__exact = request.session['username'])
+        tempUser = User.objects.get(username = request.session['username'])
         task_list1= Task.ojbects.filter(owner = tempUser)
         task_list2= []
         for a in task_list:
